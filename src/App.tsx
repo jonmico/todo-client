59e2352fad83c38a8aa3678c028b7a29e5c26a76
Jon Mico <jonmico@gmail.com>
2026-08-11 06:20:10 -0400

    Starts styling on GlobalNav.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router";
 import GlobalNav from "./components/GlobalNav";
+import styles from "./App.module.css";
 
 export default function App() {
   return (
-    <>
+    <div className={styles.app}>
       <GlobalNav />
       <Outlet />
-    </>
+    </div>
   );
 }
--- src/components/GlobalNav.tsx
+++ src/components/GlobalNav.tsx
@@ -1,18 +1,23 @@
 import { Link, NavLink } from "react-router";
+import styles from "./GlobalNav.module.css";
 
 export default function GlobalNav() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">TodoApp</Link>
-        </li>
-        <li>
-          <NavLink to="/login">Login</NavLink>
-        </li>
-        <li>
-          <NavLink to="/register">Register</NavLink>
-        </li>
+    <nav className={styles.nav}>
+      <ul className={styles.navList}>
+        <div>
+          <li className={styles.todoApp}>
+            <Link to="/">TodoApp</Link>
+          </li>
+        </div>
+        <div className={styles.loginRegisterWrapper}>
+          <li className={styles.navLink}>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+          <li className={styles.navLink}>
+            <NavLink to="/register">Register</NavLink>
+          </li>
+        </div>
       </ul>
     </nav>
   );
