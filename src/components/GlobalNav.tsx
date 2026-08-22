aff242d3101d6da833776f4b3305e3bc5a9ea5e0
Jon Mico <jonmico@gmail.com>
2026-08-22 07:55:12 -0400

    Adds logout to GlobalNav.

--- src/components/GlobalNav.tsx
+++ src/components/GlobalNav.tsx
@@ -1,10 +1,24 @@
-import { Link, NavLink } from "react-router";
+import { Link, NavLink, useNavigate } from "react-router";
 import styles from "./GlobalNav.module.css";
+import { useAuth } from "../hooks/useAuth";
 
 // TODO: Conditionally render Login/Register/Logout depending on isLoggedIn state.
 // TODO: Create and hookup logout button.
 
 export default function GlobalNav() {
+  const { isLoading, isLoggedIn, logout } = useAuth();
+  const navigate = useNavigate();
+
+  async function handleLogout() {
+    const data = await logout();
+
+    if (data.ok) {
+      return navigate("/login");
+    } else {
+      console.error(data.error);
+    }
+  }
+
   return (
     <div className={styles.navWrapper}>
       <nav className={styles.nav}>
@@ -13,14 +27,22 @@ export default function GlobalNav() {
             <Link to="/">TodoApp</Link>
           </li>
         </ul>
-        <ul className={styles.loginRegisterWrapper}>
-          <li className={styles.navLink}>
-            <NavLink to="/login">Login</NavLink>
-          </li>
-          <li className={styles.navLink}>
-            <NavLink to="/register">Register</NavLink>
-          </li>
-        </ul>
+        {!isLoggedIn ? (
+          <ul className={styles.loginRegisterWrapper}>
+            <li className={styles.navLink}>
+              <NavLink to="/login">Login</NavLink>
+            </li>
+            <li className={styles.navLink}>
+              <NavLink to="/register">Register</NavLink>
+            </li>
+          </ul>
+        ) : (
+          <div>
+            <button onClick={handleLogout} className={styles.logoutButton}>
+              Logout
+            </button>
+          </div>
+        )}
       </nav>
     </div>
   );
