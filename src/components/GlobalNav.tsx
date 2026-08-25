23993bb2513678d29d2e542a57163da903f2f608
Jon Mico <jonmico@gmail.com>
2026-08-25 06:35:58 -0400

    Adds AuthNav.

--- src/components/GlobalNav.tsx
+++ src/components/GlobalNav.tsx
@@ -2,11 +2,25 @@ import { Link, NavLink, useNavigate } from "react-router";
 import styles from "./GlobalNav.module.css";
 import { useAuth } from "../hooks/useAuth";
 
-// TODO: Conditionally render Login/Register/Logout depending on isLoggedIn state.
-// TODO: Create and hookup logout button.
-
 export default function GlobalNav() {
-  const { isLoading, isLoggedIn, logout } = useAuth();
+  const { isLoading, isLoggedIn } = useAuth();
+
+  return (
+    <div className={styles.navWrapper}>
+      <nav className={styles.nav}>
+        <ul>
+          <li className={styles.todoApp}>
+            <Link to={isLoggedIn ? "/todos" : "/"}>TodoApp</Link>
+          </li>
+        </ul>
+        {isLoading ? null : <AuthNav />}
+      </nav>
+    </div>
+  );
+}
+
+function AuthNav() {
+  const { isLoggedIn, logout } = useAuth();
   const navigate = useNavigate();
 
   async function handleLogout() {
@@ -18,32 +32,24 @@ export default function GlobalNav() {
       console.error(data.error);
     }
   }
-
   return (
-    <div className={styles.navWrapper}>
-      <nav className={styles.nav}>
-        <ul>
-          <li className={styles.todoApp}>
-            <Link to="/">TodoApp</Link>
+    <div>
+      {!isLoggedIn ? (
+        <ul className={styles.loginRegisterWrapper}>
+          <li className={styles.navLink}>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+          <li className={styles.navLink}>
+            <NavLink to="/register">Register</NavLink>
           </li>
         </ul>
-        {!isLoggedIn ? (
-          <ul className={styles.loginRegisterWrapper}>
-            <li className={styles.navLink}>
-              <NavLink to="/login">Login</NavLink>
-            </li>
-            <li className={styles.navLink}>
-              <NavLink to="/register">Register</NavLink>
-            </li>
-          </ul>
-        ) : (
-          <div>
-            <button onClick={handleLogout} className={styles.logoutButton}>
-              Logout
-            </button>
-          </div>
-        )}
-      </nav>
+      ) : (
+        <div>
+          <button onClick={handleLogout} className={styles.logoutButton}>
+            Logout
+          </button>
+        </div>
+      )}
     </div>
   );
 }
